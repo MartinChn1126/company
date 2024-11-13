6bf0744e26e9204d383672783f307fa28a626d20
MartinChn1126 <martin_chn1126@outlook.com>
2024-11-13 17:13:06 +0800

    init repository

---
+++ initialization.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import sqlite3
+from enum import Enum
+
+def web_initialization():
+    pass
+
+
+def generate_pages():
+    account_pages = {'Account':[],'Job':[],'management':[]}
+    pass
